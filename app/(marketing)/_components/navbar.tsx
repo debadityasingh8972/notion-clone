8e1e55e074fe0ccc21ecf95f39c452c14be4f848
Debaditya <debadityasingh9@gmail.com>
2023-10-30 11:56:55 +0530

    Authentication added

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,15 +1,17 @@
 "use client"
 
 import { useConvexAuth } from "convex/react";
-import { SignIn, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
 import { useScrollTop } from "@/hooks/use-scroll-top"
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./logo";
-import { Button } from "@/components/ui/button";
+
 
 export const Navbar = () => {
     const { isAuthenticated, isLoading } = useConvexAuth();
@@ -22,7 +24,7 @@ export const Navbar = () => {
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
-                    <p>Loading...</p>
+                    <Spinner />
                 )}
                 {!isAuthenticated && !isLoading && (
                     <>
@@ -36,6 +38,18 @@ export const Navbar = () => {
                                 Get Jotion free
                             </Button>
                         </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Jotion
+                            </Link>
+                        </Button>
+                        <UserButton
+                            afterSignOutUrl="/"
+                        />
                     </>
                 )}
                 <ModeToggle />
